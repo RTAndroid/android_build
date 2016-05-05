44f3cc57ac8989625e803957fae3787798b10215
Igor Kalkov <ikalkov@users.noreply.github.com>
2016-05-05 15:37:16 +0200

    Allow builds with OpenJDK 8

--- core/main.mk
+++ core/main.mk
@@ -146,13 +146,12 @@ endif
 java_version_str := $(shell unset _JAVA_OPTIONS JAVA_TOOL_OPTIONS && java -version 2>&1)
 javac_version_str := $(shell unset _JAVA_OPTIONS JAVA_TOOL_OPTIONS && javac -version 2>&1)
 
-# Check for the correct version of java, should be 1.7 by
-# default, and 1.6 if LEGACY_USE_JAVA6 is set.
+# Check for the correct version of java
 ifeq ($(LEGACY_USE_JAVA6),)
-required_version := "1.7.x"
-required_javac_version := "1.7"
-java_version := $(shell echo '$(java_version_str)' | grep '^java .*[ "]1\.7[\. "$$]')
-javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.7[\. "$$]')
+required_version := "1.[7/8].x"
+required_javac_version := "1.[7/8]"
+java_version := $(shell echo '$(java_version_str)' | grep '^openjdk .*[ "]1\.[78][\. "$$]')
+javac_version := $(shell echo '$(javac_version_str)' | grep '[ "]1\.[78][\. "$$]')
 else # if LEGACY_USE_JAVA6
 required_version := "1.6.x"
 required_javac_version := "1.6"
